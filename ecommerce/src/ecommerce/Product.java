d2a79065d753cc73ae83344bcfa373c479bdb2ef
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-24 19:04:45 -0300

    New methods were created

--- ecommerce/src/ecommerce/Product.java
+++ ecommerce/src/ecommerce/Product.java
@@ -1,6 +1,5 @@
 package ecommerce;
 
-
 public class Product {
     int source;
     String description; //They're all attributes
@@ -10,5 +9,19 @@ public class Product {
         System.out.println("----- PRODUCT ON OFFER -----");
         System.out.println(source + ": " + description);
         System.out.printf("R$ %.2f\n", price);
+    }
+
+    void changePrice(double newPrice){
+        price = newPrice;
+    }
+
+    void applyDiscount(double percent /*parameter*/){
+        price = price - ((price * percent)/100);
+    }
+
+    double simulateDiscount(double percent){
+        double priceSimulated;
+        priceSimulated = price - ((price * percent)/100);
+        return priceSimulated;
     }
 }
