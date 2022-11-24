92a852f88d7e18e7f9eedbe6a7851f70c1ecf0c3
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-24 20:14:17 -0300

    Improvements in the Store class code

--- ecommerce/src/ecommerce/Store.java
+++ ecommerce/src/ecommerce/Store.java
@@ -2,21 +2,9 @@ package ecommerce;
 
 public class Store {
     public static void main(String[] args) {
-        Product product1 = new Product();
-        Product product2 = new Product();
-        Product product3 = new Product();
-
-        product1.source = 123;
-        product1.description = "Notebook";
-        product1.price = 2500.00;
-
-        product2.source = 1234;
-        product2.description = "Mouse";
-        product2.price = 70.00;
-
-        product3.source = 1235;
-        product3.description = "Keyboard";
-        product3.price = 120.00;
+        Product product1 = new Product(123, "Notebook", 2500.00);
+        Product product2 = new Product(1234,"Mouse", 70.00);
+        Product product3 = new Product(1235, "Keyboard", 120.00);
 
         product1.showAd();
         product2.showAd();
@@ -26,15 +14,14 @@ public class Store {
         product1.applyDiscount(10);
         product1.showAd();
 
-        product2.changePrice(55.00);
+        product2.setPrice(55.00);
         product2.showAd();
 
-        product3.changePrice(99.90);
+        product3.setPrice(99.90);
         product3.showAd();
 
-        System.out.println("Discount Simulation");
-        double priceP = product1.simulateDiscount(15);
-        System.out.printf("New price: R$ %.2f\n", priceP);
+        System.out.println("Discount Simulation " + product1.getDescription());
+        System.out.printf("New price: R$ %.2f\n", product1.simulateDiscount(15));
         product1.showAd();
     }
 }
