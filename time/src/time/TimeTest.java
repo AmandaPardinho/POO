97b9a370f0799f3feffc9fd469798de552450734
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-24 23:07:41 -0300

    Added code for testing

--- time/src/time/TimeTest.java
+++ time/src/time/TimeTest.java
@@ -1,5 +1,19 @@
-public class Main {
+package time;
+
+public class TimeTest {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Time time = new Time(22, 15,45);
+
+        System.out.println(time.showUniversalHour());
+        System.out.println(time.showStandardTime());
+
+        time.setTime(8,36);
+        System.out.println(time.showUniversalHour());
+        System.out.println(time.showStandardTime());
+
+        time.setTime(17);
+        System.out.println(time.showUniversalHour());
+        System.out.println(time.showStandardTime());
     }
 }
