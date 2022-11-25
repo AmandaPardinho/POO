ec32fc7840556171e005d6d993b8c195ae365a55
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-25 18:58:54 -0300

    Creating the Product class code

--- memory/src/memory/Product.java
+++ memory/src/memory/Product.java
@@ -1,5 +1,41 @@
 package memory;
 
 public class Product {
-    private int 
+    private int source;
+    private String name;
+    private double price;
+
+    public Product(int source, String name, double price) {
+        this.source = source;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getSource() {
+        return source;
+    }
+
+    public void setSource(int source) {
+        this.source = source;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String show(){
+        return source + " - " + name + " - R$ " + price;
+    }
 }
