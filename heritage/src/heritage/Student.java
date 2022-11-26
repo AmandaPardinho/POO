8d72146b38bfcd4bee49895977c610cac76cc45d
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-26 20:32:49 -0300

    New iml file

--- heritage/src/heritage/Student.java
+++ heritage/src/heritage/Student.java
@@ -1,27 +1,2 @@
-package heritage;
-
-public class Student extends Person{
-    private String registrationNumber;
-    private String course;
-
-    public String getRegistrationNumber() {
-        return registrationNumber;
-    }
-
-    public void setRegistrationNumber(String registrationNumber) {
-        this.registrationNumber = registrationNumber;
-    }
-
-    public String getCourse() {
-        return course;
-    }
-
-    public void setCourse(String course) {
-        this.course = course;
-    }
-
-    public String showInfo(){
-        return
-
-    }
+package heritage;public class Student {
 }
