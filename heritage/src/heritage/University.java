e913e4d968e6f23ec57bb23d30b7523131e298ec
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-26 23:23:46 -0300

    Student was instantiated correctly

--- heritage/src/heritage/University.java
+++ heritage/src/heritage/University.java
@@ -4,18 +4,18 @@ public class University {
     public static void main(String[] args) {
 
         Person p = new Person("Amanda", "amanda@amanda.com", "123456789");
-        p.setName("Amanda");
-        p.setEmail("amanda@amanda.com");
-        p.setPhone("123456789");
+//        p.setName("Amanda");
+//        p.setEmail("amanda@amanda.com");
+//        p.setPhone("123456789");
 
         System.out.println(p.showInfo());
 
-        Student s = new Student();
-        s.setName("Rodrigo");
-        s.setEmail("rodrigo@rodrigo.com");
-        s.setPhone("987654321");
-        s.setRegistrationNumber(666);
-        s.setCourse("Music");
+        Student s = new Student("Rodrigo", "rodrigo@rodrigo.com", "987654321", 666,"Music");
+//        s.setName("Rodrigo");
+//        s.setEmail("rodrigo@rodrigo.com");
+//        s.setPhone("987654321");
+//        s.setRegistrationNumber(666);
+//        s.setCourse("Music");
 
         System.out.println(s.showInfo());
     }
