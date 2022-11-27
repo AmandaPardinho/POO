00895e22603863be287bf9ec26d2b0aa721395d3
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-27 00:16:31 -0300

    Created SpecialAccount class and its code

--- pi_bank/src/model/SpecialAccount.java
+++ pi_bank/src/model/SpecialAccount.java
@@ -1,2 +1,32 @@
-package model;public class SpecialAccount {
+package model;
+
+public class SpecialAccount extends BankAccount{
+    private double specialLimit;
+
+    public SpecialAccount(String holderName, String cpf, int number, double balance, double specialLimit) {
+        super(holderName, cpf, number, balance);
+        this.specialLimit = specialLimit;
+    }
+
+    @Override
+    public boolean withdraw(double value){
+         if(super.balance + this.specialLimit >= value){
+             super.balance -= value;
+             return true;
+         }
+         return false;
+    }
+
+    @Override
+    public String toString(){
+        return super.toString() + ", " + "Special limit: " + this.specialLimit;
+    }
+
+    public double getSpecialLimit() {
+        return specialLimit;
+    }
+
+    public void setSpecialLimit(double specialLimit) {
+        this.specialLimit = specialLimit;
+    }
 }
