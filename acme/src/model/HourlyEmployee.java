834b039c9755a153113178d768588c6baa3e89cd
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-27 01:52:45 -0300

    Class HourlyEmployee and its code were created

--- acme/src/model/HourlyEmployee.java
+++ acme/src/model/HourlyEmployee.java
@@ -1,2 +1,33 @@
-package model;public class HourlyEmployee {
+package model;
+
+public class HourlyEmployee extends Employee{
+    private int numberHours;
+    private double hourValue;
+
+    public HourlyEmployee(int registryNumber, String name, int numberHours, double hourValue) {
+        super(registryNumber, name);
+        this.numberHours = numberHours;
+        this.hourValue = hourValue;
+    }
+
+    public int getNumberHours() {
+        return numberHours;
+    }
+
+    public void setNumberHours(int numberHours) {
+        this.numberHours = numberHours;
+    }
+
+    public double getHourValue() {
+        return hourValue;
+    }
+
+    public void setHourValue(double hourValue) {
+        this.hourValue = hourValue;
+    }
+
+    @Override
+    public double calculateSalary() {
+        return numberHours * hourValue;
+    }
 }
