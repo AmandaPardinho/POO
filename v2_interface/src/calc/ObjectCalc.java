0772d678029388b0e4f6f771ab86540682625fbe
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-27 02:44:44 -0300

    ObjectCalc class and its code were created

--- v2_interface/src/calc/ObjectCalc.java
+++ v2_interface/src/calc/ObjectCalc.java
@@ -1,2 +1,20 @@
-package calc;public class ObjectCalc {
+package calc;
+
+import module.ModuleInterface1;
+import module.ModuleIntergace2;
+
+public class ObjectCalc implements ModuleInterface1, ModuleIntergace2 {
+    public void performsCalc(){
+        System.out.println("Calculated");
+    }
+
+    @Override
+    public void calcValues() {
+        performsCalc();
+    }
+
+    @Override
+    public void executeCalc() {
+        performsCalc();
+    }
 }
