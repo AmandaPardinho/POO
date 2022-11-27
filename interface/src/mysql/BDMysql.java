88510befbf9e8cb0d13bbcb54543d513e1fe349a
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-27 02:48:03 -0300

    BDMysql interface and its code were created

--- interface/src/mysql/BDMysql.java
+++ interface/src/mysql/BDMysql.java
@@ -1,2 +1,20 @@
-package mysql;public class BDMysql {
+package mysql;
+
+import bd_interface.InterfaceBD;
+
+public class BDMysql implements InterfaceBD {
+    @Override
+    public void connect() {
+        System.out.println("Connecting to MySQL Server");
+    }
+
+    @Override
+    public void disconnect() {
+        System.out.println("Disconnecting in MySQL Server");
+    }
+
+    @Override
+    public void execute(String command) {
+        System.out.println("ms-access> " + command);
+    }
 }
