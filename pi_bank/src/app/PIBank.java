0fab3837dd2cb43eb80bffd2848c38198e9c9192
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-27 00:21:49 -0300

    Showing the importance of instantiation

--- pi_bank/src/app/PIBank.java
+++ pi_bank/src/app/PIBank.java
@@ -6,11 +6,12 @@ import model.SpecialAccount;
 public class PIBank {
     public static void main(String[] args) {
         BankAccount b1 = new BankAccount("Amanda", "55588877711", 963741, 200.00);
-        SpecialAccount sA1 = new SpecialAccount("Lara", "44400066677", 147369, 500.00,
+        //The declaration is important, but what defines the behavior is the instantiation
+        BankAccount b2 = new SpecialAccount("Lara", "44400066677", 147369, 500.00,
                 500.00);
 
         System.out.println(b1);
-        System.out.println(sA1);
+        System.out.println(b2);
 
     }
 }
