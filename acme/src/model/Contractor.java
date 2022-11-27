419d7f0ebfe7d8464f4ed23187da54d3c35e2625
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-27 01:54:23 -0300

    Contractor class and its code were created

--- acme/src/model/Contractor.java
+++ acme/src/model/Contractor.java
@@ -1,2 +1,23 @@
-package model;public class Contractor {
+package model;
+
+public class Contractor extends Employee{
+    private double contractJob;
+
+    public Contractor(int registryNumber, String name, double contractJob) {
+        super(registryNumber, name);
+        this.contractJob = contractJob;
+    }
+
+    public double getContractJob() {
+        return contractJob;
+    }
+
+    public void setContractJob(double contractJob) {
+        this.contractJob = contractJob;
+    }
+
+    @Override
+    public double calculateSalary() {
+        return contractJob;
+    }
 }
