ab4f7b7db788f03a775f3fde6979e075fa03afaf
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-27 01:01:45 -0300

    Created Commissioner class and its code

--- acme/src/model/Commissioner.java
+++ acme/src/model/Commissioner.java
@@ -1,2 +1,33 @@
-package model;public class Commissioner {
+package model;
+
+public class Commissioner extends Employee{
+    private double baseSalary;
+    private double commission;
+
+    public Commissioner(int registryNumber, String name, double baseSalary, double commission) {
+        super(registryNumber, name);
+        this.baseSalary = baseSalary;
+        this.commission = commission;
+    }
+
+    public double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    public double getCommission() {
+        return commission;
+    }
+
+    public void setCommission(double commission) {
+        this.commission = commission;
+    }
+
+    @Override
+    public double calculateSalary() {
+        return baseSalary + ((baseSalary * commission) / 100);
+    }
 }
