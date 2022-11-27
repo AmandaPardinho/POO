5a8a39627a378c456679a75a29e275c53e7333e5
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-27 01:02:17 -0300

    Created Boss class and its code

--- acme/src/model/Boss.java
+++ acme/src/model/Boss.java
@@ -1,2 +1,43 @@
-package model;public class Boss {
+package model;
+
+public class Boss extends Employee{
+    private double baseSalary;
+    private double additionalPay;
+    private double suitBenefit;
+
+    public Boss(int registryNumber, String name, double baseSalary, double additionalPay, double suitBenefit) {
+        super(registryNumber, name);
+        this.baseSalary = baseSalary;
+        this.additionalPay = additionalPay;
+        this.suitBenefit = suitBenefit;
+    }
+
+    public double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    public double getAdditionalPay() {
+        return additionalPay;
+    }
+
+    public void setAdditionalPay(double additionalPay) {
+        this.additionalPay = additionalPay;
+    }
+
+    public double getSuitBenefit() {
+        return suitBenefit;
+    }
+
+    public void setSuitBenefit(double suitBenefit) {
+        this.suitBenefit = suitBenefit;
+    }
+
+    @Override
+    public double calculateSalary() {
+        return baseSalary + ((baseSalary * additionalPay) / 100) + suitBenefit;
+    }
 }
