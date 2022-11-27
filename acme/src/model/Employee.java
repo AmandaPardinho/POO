03d46c987c7519aaee47a92d2f140bbb9349b678
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-27 01:00:15 -0300

    Created Employee class and its code

--- acme/src/model/Employee.java
+++ acme/src/model/Employee.java
@@ -1,2 +1,30 @@
-package model;public class employee {
+package model;
+
+public abstract class Employee {
+    protected int registryNumber;
+    protected String name;
+
+    public Employee(int registryNumber, String name) {
+        super();
+        this.registryNumber = registryNumber;
+        this.name = name;
+    }
+
+    public abstract double calculateSalary();
+
+    public int getRegistryNumber() {
+        return registryNumber;
+    }
+
+    public void setRegistryNumber(int registryNumber) {
+        this.registryNumber = registryNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
