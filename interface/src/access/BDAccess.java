020fd2af835e38b557ca85572329f2221643dfb1
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-27 02:49:27 -0300

    BDAccess interface and its code were created

--- interface/src/access/BDAccess.java
+++ interface/src/access/BDAccess.java
@@ -1,2 +1,20 @@
-package access;public class BDAccess {
+package access;
+
+import bd_interface.InterfaceBD;
+
+public class BDAccess implements InterfaceBD {
+    @Override
+    public void connect() {
+        System.out.println("Connecting to Access database");
+    }
+
+    @Override
+    public void disconnect() {
+        System.out.println("Disconnecting in Access Database");
+    }
+
+    @Override
+    public void execute(String command) {
+        System.out.println("ms-access> " + command);
+    }
 }
