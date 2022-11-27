136242eb0ad674be9906e88c2a236260b6cf7f8c
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-26 23:50:21 -0300

    Created BankAccount class and its code

--- pi_bank/src/model/BankAccount.java
+++ pi_bank/src/model/BankAccount.java
@@ -1,2 +1,54 @@
+package model;
+
 public class BankAccount {
+    protected String holderName;
+    protected String cpf;
+    protected int number;
+    protected double balance;
+
+    public BankAccount(String holderName, String cpf, int number, double balance) {
+        super();
+        this.holderName = holderName;
+        this.cpf = cpf;
+        this.number = number;
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString(){
+        return "Name: " + holderName + ", " + "CPF: " + cpf + ", " + "Account Number: " + number + ", " + "Balance: " +
+                balance;
+    }
+
+    public String HolderName() {
+        return holderName;
+    }
+
+    public void setHolderName(String holderName) {
+        this.holderName = holderName;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
 }
