bb9bee4386298efee48581646b2c1c48a5657f6f
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-27 02:48:47 -0300

    InterfaceBD interface and its code were created

--- interface/src/bd_interface/InterfaceBD.java
+++ interface/src/bd_interface/InterfaceBD.java
@@ -1,2 +1,8 @@
-package bd_interface;public interface InterfaceBD {
+package bd_interface;
+
+public interface InterfaceBD {
+    //Interface: classe abstrata que contém métodos abstratos
+    public void connect();
+    public void disconnect();
+    public void execute(String command);
 }
