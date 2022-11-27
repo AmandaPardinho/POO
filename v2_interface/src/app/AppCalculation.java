c9d29c30b1c87c3a7fe7d45832398c73b59a0d8f
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-27 02:45:31 -0300

    AppCalculation class and its code were created

--- v2_interface/src/app/AppCalculation.java
+++ v2_interface/src/app/AppCalculation.java
@@ -1,2 +1,21 @@
-package app;public class AppCalculation {
+package app;
+
+import calc.ObjectCalc;
+import module.ModuleInterface1;
+import module.ModuleIntergace2;
+
+public class AppCalculation {
+    public static void main(String[] args) {
+        //Situation 1 - the object's method is called as it is
+        ObjectCalc oc = new ObjectCalc();
+        oc.performsCalc();
+
+        //Situation 2 - the X module can only call via calcValues() method
+        ModuleInterface1 mi1 = oc;
+        mi1.calcValues();
+
+        //Situation 2 - the Y module can only call via executeCalc() method
+        ModuleIntergace2 mi2 = oc;
+        mi2.executeCalc();
+    }
 }
