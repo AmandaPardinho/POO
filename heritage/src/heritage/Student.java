7ab0451ac57daa7864a6931c36dbb53d3123fcc1
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-26 23:15:27 -0300

    Constructor method was created

--- heritage/src/heritage/Student.java
+++ heritage/src/heritage/Student.java
@@ -4,6 +4,12 @@ public class Student extends Person {
     private String registrationNumber;
     private String course;
 
+    public Student(String name, String email, String phone, String registrationNumber, String course) {
+        super(name, email, phone);
+        this.registrationNumber = registrationNumber;
+        this.course = course;
+    }
+
     public String getRegistrationNumber() {
         return registrationNumber;
     }
