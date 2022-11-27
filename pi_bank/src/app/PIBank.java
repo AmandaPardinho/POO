840d726b6a2452b2aa2155973f1ec757e2789e7c
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-27 00:17:35 -0300

    Instantiating SpecialAccount

--- pi_bank/src/app/PIBank.java
+++ pi_bank/src/app/PIBank.java
@@ -1,12 +1,16 @@
 package app;
 
 import model.BankAccount;
+import model.SpecialAccount;
 
 public class PIBank {
     public static void main(String[] args) {
         BankAccount b1 = new BankAccount("Amanda", "55588877711", 963741, 200.00);
+        SpecialAccount sA1 = new SpecialAccount("Lara", "44400066677", 147369, 500.00,
+                500.00);
 
         System.out.println(b1);
+        System.out.println(sA1);
 
     }
 }
